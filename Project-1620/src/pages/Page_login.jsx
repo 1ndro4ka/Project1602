11a3b9b0fff1b942132a490d1dc3bfca707d10fa
1ndro4ka <milk_2020@mail.ru>
2026-02-18 17:13:07 +0500

    Забыл вчера коммитнуть, сделал рабочую форму которая сохраняет данные в localstorage

--- Project-1620/src/pages/Page_login.jsx
+++ Project-1620/src/pages/Page_login.jsx
@@ -1,13 +1,34 @@
-function Page_login() {
-    return(
-        <form>
-            <h1>Вход</h1>
-            <input type="text" placeholder="Логин" />
-            <input type="password" placeholder="Пароль" />
-            <button type="submit">Войти</button>
-        </form>
-    )
+import { useState } from "react";
 
+function Page_login() {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem("login", login);
+    localStorage.setItem("password", password);
+    alert("test");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Вход</h1>
+      <input
+        type="text"
+        placeholder="Логин"
+        value={login}
+        onChange={(e) => setLogin(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Пароль"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Войти</button>
+    </form>
+  );
 }
 
 export default Page_login;
